29878482cf03be5278463afe2efcf6c112402fa7
Rob Toombs <rtoombs@protonmail.com>
2023-02-25 13:31:37 -0500

    Added some description comments

--- src/main/kotlin/ListNode_2.kt
+++ src/main/kotlin/ListNode_2.kt
@@ -24,6 +24,19 @@ class ListNode(value: Int) {
         return "$result]"
     }
 
+    /**
+    Knowing the provided ListNodes are in 'reverse order' where
+    the first node represents the ones position, build up the BigInt
+    multiply each 'n' by 10^n before adding it to the sum.
+
+    Ex. [9,1,3,4]
+    9 * 10^0 = 9
+    1 * 10^1 = 10
+    3 * 10^2 = 300
+    4 * 10^3 = 4000
+
+    4000 + 300 + 10 + 9 = 4319
+     */
     fun toBigInt(): BigInteger {
         var result = BigInteger.valueOf(`val`.toLong())
 
@@ -40,7 +53,12 @@ class ListNode(value: Int) {
     }
 
     companion object {
-        fun toReversedNode(l: BigInteger): ListNode? {
+        /**
+        Take the given BitInteger, convert it to a character array, and
+        iterate through it backwards (since the head of the ListNode is the
+        last digit of the number) building up a new ListNode
+         */
+        fun toListNode(l: BigInteger): ListNode? {
             var result: ListNode? = null
 
             val number = "$l".toCharArray()
@@ -68,7 +86,7 @@ fun main() {
     l2.add(6)
     l2.add(4)
 
-    println("${ListNode.toReversedNode(l1.toBigInt() + l2.toBigInt())} = [7,0,8]")
+    println("${ListNode.toListNode(l1.toBigInt() + l2.toBigInt())} = [7,0,8]")
 
     l1 = ListNode(2)
     l1.add(4)
@@ -79,7 +97,7 @@ fun main() {
     l2.add(4)
     l2.add(9)
 
-    println("${ListNode.toReversedNode(l1.toBigInt() + l2.toBigInt())} = [7,0,4,0,1]")
+    println("${ListNode.toListNode(l1.toBigInt() + l2.toBigInt())} = [7,0,4,0,1]")
 
     l1 = ListNode(9)
     l1.add(9)
@@ -94,7 +112,7 @@ fun main() {
     l2.add(9)
     l2.add(9)
 
-    println("${ListNode.toReversedNode(l1.toBigInt() + l2.toBigInt())} = [8,9,9,9,0,0,0,1]")
+    println("${ListNode.toListNode(l1.toBigInt() + l2.toBigInt())} = [8,9,9,9,0,0,0,1]")
 
     l1 = ListNode(9)
 
@@ -109,7 +127,7 @@ fun main() {
     l2.add(9)
     l2.add(9)
 
-    println("${ListNode.toReversedNode(l1.toBigInt() + l2.toBigInt())} = [0,0,0,0,0,0,0,0,0,0,1]")
+    println("${ListNode.toListNode(l1.toBigInt() + l2.toBigInt())} = [0,0,0,0,0,0,0,0,0,0,1]")
 
     l1 = ListNode(1)
     l1.add(1)
@@ -135,7 +153,7 @@ fun main() {
     l2.add(1)
     l2.add(1)
 
-    println("${ListNode.toReversedNode(l1.toBigInt() + l2.toBigInt())} = [2,2,2,2,2,2,2,2,2,2,2]")
+    println("${ListNode.toListNode(l1.toBigInt() + l2.toBigInt())} = [2,2,2,2,2,2,2,2,2,2,2]")
 
     l1 = ListNode(1)
     l1.add(0)
@@ -173,5 +191,5 @@ fun main() {
     l2.add(6)
     l2.add(4)
 
-    println("${ListNode.toReversedNode(l1.toBigInt() + l2.toBigInt())} = [6,6,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]")
+    println("${ListNode.toListNode(l1.toBigInt() + l2.toBigInt())} = [6,6,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]")
 }
